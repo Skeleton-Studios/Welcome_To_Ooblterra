049dd417176fd5d2628b0489c4fd536b3be8dd70
Skull <33642481+Skull220@users.noreply.github.com>
2024-01-22 16:10:50 -0500

    eyesec deepscan added

---
+++ Things/TeslaCoil.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unity.Netcode;
+using UnityEngine;
+
+namespace Welcome_To_Ooblterra.Things {
+    internal class TeslaCoil : NetworkBehaviour {
+        public bool TeslaCoilOn;
+    }
+}
