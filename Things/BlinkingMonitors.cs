e8f3bd002a274b596d28c124d6d0924e2abc09a3
Skull220 <33642481+Skull220@users.noreply.github.com>
2024-02-14 17:56:31 -0500

    Fixed the lag spike problem (for real i think this was it)

--- Things/BlinkingMonitors.cs
+++ Things/BlinkingMonitors.cs
@@ -9,34 +9,4 @@ using UnityEngine;
 namespace Welcome_To_Ooblterra.Things;
 internal class BlinkingMonitors : MonoBehaviour {
 
-    System.Random BlinkRandom;
-    bool ShouldBlink;
-    int BlinkTimer;
-    public Material EyeMaterial;
-    public MeshRenderer MonitorMesh;
-
-    public void Awake() {
-        MonitorMesh.materials[1] = EyeMaterial;
-        BlinkRandom = new System.Random();
-    }
-    public void Update() {
-        if(ShouldBlink) {
-            StartCoroutine(BlinkEye());
-        }
-        if(BlinkRandom.Next(0, 100) < 99) {
-            return;
-        }
-
-    }
-    IEnumerator BlinkEye() {
-        if(BlinkTimer < 5) {
-            EyeMaterial.SetInt("_ShouldBlink", 1);
-            BlinkTimer++;
-        } else {
-            EyeMaterial.SetInt("_ShouldBlink", 0);
-            BlinkTimer = 0;
-            StopCoroutine(BlinkEye());
-        }
-        yield return null;
-    }
 }
