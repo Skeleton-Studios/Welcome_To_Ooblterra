11193a7c298c1cb5af53c341c82815e925610972
Skull220 <33642481+Skull220@users.noreply.github.com>
2024-04-23 12:05:21 -0400

    working on new baby lurker

--- Enemies/BabyLurkerAI.cs
+++ Enemies/BabyLurkerAI.cs
@@ -3,209 +3,126 @@ using UnityEngine;
 using GameNetcodeStuff;
 using UnityEngine.AI;
 using UnityEngine.Assertions.Must;
+using System.Runtime.CompilerServices;
 
 namespace Welcome_To_Ooblterra.Enemies;
 public class BabyLurkerAI : WTOEnemy {
-    /*
-    //BEHAVIOR STATES
+
+    [Header("Defaults")]
+    public bool ThrowProjectile;
+    public int ProjectilesThrown;
+    public GameObject projectileTemplate;
+    public float launchVelocity = 100f;
+    public Transform LaunchTransform;
+
+
+    //STATES
     private class Spawn : BehaviorState {
 
         public override void OnStateEntered(int enemyIndex, System.Random enemyRandom, Animator creatureAnimator) {
-            creatureAnimator.SetBool("Spawn", value: true);
+
         }
         public override void UpdateBehavior(int enemyIndex, System.Random enemyRandom, Animator creatureAnimator) {
 
         }
         public override void OnStateExit(int enemyIndex, System.Random enemyRandom, Animator creatureAnimator) {
-            creatureAnimator.SetBool("Spawn", value: false);
-        }
-        public override List<StateTransition> transitions { get; set; } = new List<StateTransition> {
-            new SpawnTransition(55)
-        };
-    }
-    private class Roam : BehaviorState {
-        bool canMakeNextPoint;
-        public override void OnStateEntered(int enemyIndex, System.Random enemyRandom, Animator creatureAnimator) {
-            self.creatureAnimator.SetBool("Moving", true);
-            canMakeNextPoint = self.SetDestinationToPosition(RoundManager.Instance.GetRandomNavMeshPositionInRadius(self.allAINodes[enemyRandom.Next(self.allAINodes.Length - 1)].transform.position, 5), checkForPath: true);
-            self.agent.speed = 7f;
-        }
-        public override void UpdateBehavior(int enemyIndex, System.Random enemyRandom, Animator creatureAnimator) {
-            if (!canMakeNextPoint) {
-                canMakeNextPoint = self.SetDestinationToPosition(RoundManager.Instance.GetRandomNavMeshPositionInRadius(self.allAINodes[enemyRandom.Next(self.allAINodes.Length - 1)].transform.position, 5), checkForPath: true);
-            }
-        }
-        public override void OnStateExit(int enemyIndex, System.Random enemyRandom, Animator creatureAnimator) {
-        }
-        public override List<StateTransition> transitions { get; set; } = new List<StateTransition> { 
-            new EnemySpottedTransition()
-        };
-    }
-    private class Lunge : BehaviorState {
-        private Ray ray;
-        private RaycastHit rayHit;
-        public override void OnStateEntered(int enemyIndex, System.Random enemyRandom, Animator creatureAnimator) {
 
-            Instance.LungeComplete = false;
-            self.creatureAnimator.SetBool("Attacking", true);
-            ray = new Ray(self.transform.position + Vector3.up, self.transform.forward);
-            Vector3 pos = ((!Physics.Raycast(ray, out rayHit, 17f, StartOfRound.Instance.collidersAndRoomMask)) ? ray.GetPoint(17f) : rayHit.point);
-            pos = Instance.roundManager.GetNavMeshPosition(pos);
-            self.SetDestinationToPosition(self.targetPlayer.transform.position);
-            self.agent.speed = 13f;
-        }
-        public override void UpdateBehavior(int enemyIndex, System.Random enemyRandom, Animator creatureAnimator) {
-                
-            self.agent.speed -= Time.deltaTime * 5f;
-            if (self.agent.speed < 1.5f){
-                Instance.LungeComplete = true;
-                if(Instance.LungeCooldown < 1) { 
-                    Instance.LungeCooldown = 500;
-                }
-            }
-        }
-        public override void OnStateExit(int enemyIndex, System.Random enemyRandom, Animator creatureAnimator) {
-            self.creatureAnimator.SetBool("Attacking", false);
-        }
-        public override List<StateTransition> transitions { get; set; } = new List<StateTransition> { 
-            new EnemyKilledTransition(),
-            new EnemyAliveTransition()
-        };
-    }
-    private class Reposition : BehaviorState {
-        bool isRepositioning;
-        public override void OnStateEntered(int enemyIndex, System.Random enemyRandom, Animator creatureAnimator) {
-            self.creatureAnimator.SetBool("Moving", true);
-            isRepositioning = self.SetDestinationToPosition(self.ChooseClosestNodeToPosition(self.targetPlayer.transform.position).position);
-            self.agent.speed = 8f;
-        }
-        public override void UpdateBehavior(int enemyIndex, System.Random enemyRandom, Animator creatureAnimator) {
-            if (!isRepositioning) {
-                isRepositioning = self.SetDestinationToPosition(self.ChooseClosestNodeToPosition(self.targetPlayer.transform.position).position);
-            }
-        }
-        public override void OnStateExit(int enemyIndex, System.Random enemyRandom, Animator creatureAnimator) {
-            self.creatureAnimator.SetBool("Moving", false);
         }
         public override List<StateTransition> transitions { get; set; } = new List<StateTransition> {
-            new InPositionTransition(),
-            new EnemyOutOfRange()
+            new TargetPlayerInLOS(),
+            new TargetPlayerNotInLOS()
         };
+
     }
-    private class StayOnPlayer : BehaviorState {
+    private class ChasePlayer : BehaviorState {
+
         public override void OnStateEntered(int enemyIndex, System.Random enemyRandom, Animator creatureAnimator) {
-            self.creatureAnimator.SetBool("Moving", false);
+            BabyLurkerList[enemyIndex].SetDestinationToPosition(BabyLurkerList[enemyIndex].targetPlayer.transform.position);
         }
         public override void UpdateBehavior(int enemyIndex, System.Random enemyRandom, Animator creatureAnimator) {
-                
+
         }
         public override void OnStateExit(int enemyIndex, System.Random enemyRandom, Animator creatureAnimator) {
+
         }
         public override List<StateTransition> transitions { get; set; } = new List<StateTransition> {
-            new SpawnTransition(90),
+            new AnyPlayerInLOS()
         };
+
+    }
+    private class ThrowSelfAtPlayer : BehaviorState {
+
+        public override void OnStateEntered(int enemyIndex, System.Random enemyRandom, Animator creatureAnimator) {
+            BabyLurkerList[enemyIndex].LaunchProjectile(BabyLurkerList[enemyIndex].LaunchTransform);
+        }
+        public override void UpdateBehavior(int enemyIndex, System.Random enemyRandom, Animator creatureAnimator) {
+
+        }
+        public override void OnStateExit(int enemyIndex, System.Random enemyRandom, Animator creatureAnimator) {
+
+        }
+        public override List<StateTransition> transitions { get; set; } = new List<StateTransition> {
+
+        };
+
     }
 
-    //STATE TRANSITIONS
-    private class SpawnTransition : StateTransition {
-        private int SpawnTime;
-        private int TotalTime = 55;
-        public SpawnTransition(int newtime){
-            TotalTime = newtime;
-        }
+    //TRANSITIONS
+    private class TargetPlayerInLOS : StateTransition {
         public override bool CanTransitionBeTaken() {
-            if (SpawnTime < TotalTime) {
-                SpawnTime++;
-                return false;
-            }
-            return true;
+            return BabyLurkerList[enemyIndex].IsTargetPlayerWithinLOS(range: 10, width: 90);
         }
         public override BehaviorState NextState() {
-            if (Instance.IsEnemyInRange()) {
-                return new Lunge();
-            }
-            return new Roam();
+            return new ThrowSelfAtPlayer();
         }
     }
-    private class InPositionTransition : StateTransition {
+    private class AnyPlayerInLOS : StateTransition {
         public override bool CanTransitionBeTaken() {
-                
-            return (Vector3.Distance(Instance.transform.position, Instance.destination) < 3) && Instance.LungeCooldown <= 0; 
-        }
-        public override BehaviorState NextState() {
-                
-            if (Instance.IsEnemyInRange()) {
-                return new Lunge();
-            } 
-            return new Roam();
-        }
-    }
-    private class EnemyOutOfRange : StateTransition {
-        public override bool CanTransitionBeTaken() {
-                
-            if ((Vector3.Distance(Instance.transform.position, Instance.targetPlayer.transform.position) > 15)){
-                Instance.targetPlayer = null;
-                return true;
-            }
             return false;
+            //return BabyLurkerList[enemyIndex].IsAnyPlayerWithinLOS(range: 10, width: 90);
         }
         public override BehaviorState NextState() {
-                
-            return new Roam();
+            return new ThrowSelfAtPlayer();
         }
     }
-    private class EnemySpottedTransition : StateTransition {
+    private class TargetPlayerNotInLOS : StateTransition {
         public override bool CanTransitionBeTaken() {
-            Instance.targetPlayer = Instance.CheckLineOfSightForClosestPlayer(180f, 60, 2);
-            return (Instance.targetPlayer != null);
+            return !BabyLurkerList[enemyIndex].IsTargetPlayerWithinLOS(range: 10, width: 90); 
         }
         public override BehaviorState NextState() {
-            return new Reposition();
-        }
-    }
-    private class EnemyAliveTransition : StateTransition {
-        public override bool CanTransitionBeTaken() {
-                
-            return Instance.targetPlayer.health > 0 && Instance.LungeComplete;
-        }
-        public override BehaviorState NextState() {
-            return new Reposition();
-        }
-    }
-    private class EnemyKilledTransition : StateTransition {
-        public override bool CanTransitionBeTaken() {
-                
-            return Instance.targetPlayer.health <= 0 && Instance.LungeComplete;
-        }
-        public override BehaviorState NextState() {
-            return new StayOnPlayer();
+            return new ChasePlayer();
         }
     }
 
-    private bool LungeComplete;
-    private int LungeCooldown;
-    private static BabyLurkerAI Instance;
-    public override string __getTypeName() {
-        return "BabyLurkerAI";
-    }
+    private GameObject LiveProjectile;
+
+    public static Dictionary<int, BabyLurkerAI> BabyLurkerList = new();
+    private static int BabyLurkerID;
+
     public override void Start() {
-        Instance = this;
+        MyValidState = PlayerState.Inside;
         InitialState = new Spawn();
         PrintDebugs = true;
+        BabyLurkerID++;
+        WTOEnemyID = BabyLurkerID;
+        LogMessage($"Adding BabyLurker {this} #{BabyLurkerID}");
+        BabyLurkerList.Add(BabyLurkerID, this);
         base.Start();
     }
+
     public override void Update() {
-        base.Update();
-        if(LungeCooldown > 0) {
-            LungeCooldown--;
-        }
+        if (ThrowProjectile) {
+            LaunchProjectile(LaunchTransform);
+        } 
+        //base.Update();
     }
-    private bool IsEnemyInRange() {
-        PlayerControllerB nearestPlayer = CheckLineOfSightForClosestPlayer(90f, 10, 2);
-        if (nearestPlayer != null) {
-            targetPlayer = nearestPlayer;
+    public void LaunchProjectile(Transform LaunchTransform) {
+        if(ProjectilesThrown > 0) {
+            return;
         }
-        return nearestPlayer != null;
+        ProjectilesThrown++;
+        LiveProjectile = Instantiate(projectileTemplate, LaunchTransform.position, LaunchTransform.rotation);
+        LiveProjectile.GetComponent<Rigidbody>().AddRelativeForce(new Vector3(0, launchVelocity, 0));
+        GameObject.Destroy(this);
     }
-}*/
 }
---
+++ Enemies/EnemyThings/BabyLurkerProjectile.cs
@@ -0,0 +1,32 @@
+﻿using GameNetcodeStuff;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using Welcome_To_Ooblterra.Properties;
+
+namespace Welcome_To_Ooblterra.Enemies.EnemyThings;
+public class BabyLurkerProjectile : MonoBehaviour {
+
+    public AudioSource Noisemaker;
+    public ParticleSystem ExplodeParticle;
+    public GameObject self;
+
+    private void OnTriggerEnter(Collider other) {
+        WTOBase.LogToConsole("Collision registered!");
+        PlayerControllerB victim = other.gameObject.GetComponent<PlayerControllerB>();
+        if (other.gameObject.CompareTag("Player")) {
+            victim.DamagePlayer(5, causeOfDeath: CauseOfDeath.Unknown);
+        }
+        DestroySelf();
+    }
+
+    private void DestroySelf() {
+        
+        Noisemaker?.Play();
+        ExplodeParticle?.Play();
+        Destroy(self); 
+    }
+}
