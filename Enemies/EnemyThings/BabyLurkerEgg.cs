364ccae688ca44e98e70a510c555820310c844d6
Skull220 <33642481+Skull220@users.noreply.github.com>
2024-04-23 17:31:20 -0400

    work on baby lurker egg

--- Enemies/EnemyThings/BabyLurkerEgg.cs
+++ Enemies/EnemyThings/BabyLurkerEgg.cs
@@ -1,34 +1,47 @@
-﻿using System;
+﻿using GameNetcodeStuff;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Unity.Netcode;
 using UnityEngine;
+using Welcome_To_Ooblterra.Enemies;
+using Welcome_To_Ooblterra.Patches;
 
 namespace Welcome_To_Ooblterra.Things;
 public class BabyLurkerEgg : NetworkBehaviour {
+
     private System.Random enemyRandom;
-    private int TimeUntilBabySpawns;
-    private int TimeInRange;
-    public bool BabySpawned;
+    public bool BabiesSpawned;
+    public int BabiesToSpawn = 35;
 
     private void Start() { 
         enemyRandom = new System.Random(StartOfRound.Instance.randomMapSeed);
-        TimeUntilBabySpawns = enemyRandom.Next(180, 1200);
     }
     private void OnTriggerStay(Collider other) {
-        if(BabySpawned) {
+        PlayerControllerB victim = other.gameObject.GetComponent<PlayerControllerB>();
+        if (other.gameObject.CompareTag("Player")) {
+            SpawnBabyLurkersServerRpc((int)victim.actualClientId);
+        }
+        if (BabiesSpawned) {
             return;
-        }
-        if (TimeInRange > TimeUntilBabySpawns) {
-            SpawnBabyLurker();
-        }
-        TimeInRange++;
+        }   
     }
-    private void SpawnBabyLurker() { 
-        BabySpawned = true;
-        //animate the egg opening
-        //INSTANCIATE BABY LURKER UnityEngine.Object.Instantiate()
+    [ServerRpc]
+    public void SpawnBabyLurkersServerRpc(int targetID) {
+        SpawnBabyLurkersClientRpc(targetID);
+    }
+    [ClientRpc]
+    public void SpawnBabyLurkersClientRpc(int targetID){ 
+        BabiesSpawned = true;
+        GameObject BabyLurkerPrefab = MonsterPatch.InsideEnemies.First(x => x.enemyType.enemyName == "Baby Lurker").enemyType.enemyPrefab;
+        for (int i = 0; i < BabiesToSpawn; i++) { 
+            GameObject BabyLurker = Instantiate(BabyLurkerPrefab);
+            if (base.IsServer) {
+                BabyLurker.gameObject.GetComponentInChildren<NetworkObject>().Spawn(destroyWithScene: true);
+                BabyLurker.gameObject.GetComponentInChildren<BabyLurkerAI>().SetTargetServerRpc(targetID);
+            }
+        }
     }
 }
